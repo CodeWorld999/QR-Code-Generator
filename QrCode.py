43afa24b10523fecda39ce0c14edf758db4f6d2c
CodeWorld999 <Codeworld999@gmail.com>
2021-06-16 01:47:24 +0530

    Generate your .svg QRCode

---
+++ QrCode.py
@@ -0,0 +1,13 @@
+import pyqrcode
+from pyqrcode import QRCode
+
+
+link = input("Enter the link: ")
+size = input("Size of the QRCode you want: Example-5 ")
+saveAs = input("Save as: ")
+
+# Generate QR code
+url = pyqrcode.create(link)
+
+# Create and save
+url.svg(saveAs+".svg", scale = size)
---
+++ README.md
@@ -0,0 +1,2 @@
+Make your own url to .svg QRCode
+Just need one package "pyqrcode" which you can install by hit a command "pip install pyqrcode"
